ad23b6e8afbba7595b1af2fbfeaf659b5f937e5a
benwulf <benwulf@zedat.fu-berlin.de>
2017-04-21 20:18:04 +0200

    Nearly Task2 done. Task3 missing.

--- task_01/exercise1.r
+++ task_01/exercise1.r
@@ -1,7 +1,20 @@
 #loaddata
 library(paprbag)
 library(randomForest)
-data('trainigData')
+data('trainingData')
+
+confusion <-function (data, reference, mute=F)
+{
+  equal <- data==reference
+  TP<- sum(data[equal])
+  TN<- sum(!data[equal])
+  FP<- sum(data[!equal])
+  FN<- sum(!data[!equal])
+  
+  
+}
+
+
 
 set.seed(42)  # set seed to get always the same random numbers
 
@@ -13,9 +26,27 @@ trainingselection[postive_samples_positions]<-TRUE
 trainingselection[negative_samples_positions]<-TRUE
 
 data_training<-trainingData[trainingselection,]
+
+
 data_test<-trainingData[!trainingselection,]
 
+rf_model <- randomForest(Labels ~ ., data=data_training,xtest=data_test[,-1],ytest=data_test[,1])
+
+print_results <- function (x)
+{
+  cat("Training Set\n")
+  cat(paste("Training set error:",mean(x$confusion[,3],'\n')))
+  print(x$confusion)
+
+  cat("Test Set")
+  cat(paste("Test set error:",mean(x$test$confusion[,3])))
+  print(x$confusion)
+}
+
+print_results(rf_model)
+
+print(paste("Sensitivity Train set:",rf_model ))
+
+model_test <- predict(rf_model,data=data_test)
 
 
-
-rf <- randomForest(label ~ ., data=train)
