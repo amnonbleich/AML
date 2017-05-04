3f71be0ba61e16dfce715c6074270da86cae709a
Amnon Bleich <D069165@berl33990345b.emea.global.corp.sap>
2017-05-04 17:03:43 +0200

    sort by uniPrac length + refactor

--- task_02/ex2.py
+++ task_02/ex2.py
@@ -1,38 +1,28 @@
 FILE_PATH = './all_uniparc_mapped_to_uniprot_and_proteomes.tdl'
 
-# dictionary UniPrac: tuple
+# dictionary UniPrac:uniProt
 uniPrac_dict = {}
-uniProt_dict = {}
+# dictionary proteome:uniPrac
 proteome_dict = {}
-proteome_sorted_list = []
-
-
 
 with open(FILE_PATH) as f:
-    next(f) # skipe first line
+    next(f) # skip first line
     for line in f:
         # data processing
         line = line.rstrip() # remove special characters 
         line = line.split('\t')
         
-        # create uniProt:uniPrac dict
-        if line[0] in uniProt_dict:
-            uniProt_dict[line[0]].update(set([line[1]]))
+        # create uniPrac:uniProt dict
+        if line[0] in uniPrac_dict:
+            uniPrac_dict[line[0]].update(set([line[1]]))
         else:
-            uniProt_dict[line[0]] = set([line[1]])
+            uniPrac_dict[line[0]] = set([line[1]])
 
         # protemoe exist
         if len(line) == 3:
             #extract proteoms
             proteome={x.split(':')[0] for x in line[2].split(', ')}
-            # create uniPrac:proteome dict
-            if line[0] in uniPrac_dict:
-                uniPrac_dict[line[0]].update(proteome)     
-            else:
-                uniPrac_dict[line[0]] = proteome
-           
             # create proteome:uniPrac dict
-
             for prot in proteome:
                 if prot in proteome_dict:
                     proteome_dict[prot].update(set([line[0]]))
@@ -40,51 +30,46 @@ with open(FILE_PATH) as f:
                     proteome_dict[prot] = set([line[0]])
  
     f.close()
-#
-## sort proteomes by number of uniPrac in proteome
-#for k in sorted(proteome_dict, key=lambda k: len(proteome_dict[k])):
-#        proteome_sorted_list.append(proteome_dict[k])
-#
-#
+
 
 ## Top Down Version
 
-'''Better Version of it '''
 def maxsize(dictionary,covered):
     
-    maxi=-1
-    keys=''
+    maxi = -1
+    keys = ''
     for key, value in dictionary.items():
-        l=len(value.difference(covered))
+        l = len(value.difference(covered))
         if (l>maxi):
-            maxi=l
-            keys=key
+            maxi = l
+            keys = key
     return(keys)            
-            
-                
-def reduce_max(Proteom_dict,UniParc_Dict):
-    required=list()
-    covered_UniPark=set()
-    while(len(Proteom_dict)):
-        p=maxsize(Proteom_dict,covered_UniPark)
-        unique=Proteom_dict[p].difference(covered_UniPark)
+                         
+def reduce_max(local_proteome_dict):
+    required = list()
+    covered_uniPrac = set()
+    while(len(local_proteome_dict)):
+        p = maxsize(local_proteome_dict,covered_uniPrac)
+        unique = local_proteome_dict[p].difference(covered_uniPrac)
         if(len(unique)):
-            covered_UniPark.update(unique)
+            covered_uniPrac.update(unique)
             required.append((p,unique))
             
-        Proteom_dict.pop(p)
-    print("%i proteoms given, reduced to %i, %.2f%% of input"%(len(proteome_dict),len(required),(len(required)*100/len(proteome_dict))))
+        local_proteome_dict.pop(p)
+    print("%i proteomes given, reduced to %i, %.2f%% of input"%(len(proteome_dict),len(required),(len(required)*100/len(proteome_dict))))
     return(required)
 
-zz=reduce_max(proteome_dict.copy(),uniPrac_dict.copy())
+zz=reduce_max(proteome_dict.copy())
 
-def proteome2uniprot(proteomeList,uniProt_dict):
-    ret=list()
+def proteome2uniprot(proteomeList,uniPrac_dict):
+    ret = list()
     for elem in proteomeList:
-        uprotset=set()
-        for upark in elem[1]:
-            uprotset.update(uniProt_dict[upark])
-        ret.append((elem[0],list(uprotset)))
-    #ret.sort(key=lambda tup: len(tup[1]))
+        uProt_set = set()
+        for uPrac in elem[1]:
+            uProt_set.update(uniPrac_dict[uPrac])
+        ret.append((elem[0],list(uProt_set)))
+    # sort proteomes by number of uniProt associated
+    ret = sorted(ret , key=lambda x: len(x[1]), reverse=True)
     return(ret)
-            
+
+proteome_sorted_list = proteome2uniprot(zz, uniPrac_dict)
