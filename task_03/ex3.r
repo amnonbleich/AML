eb29c2c24755be28611b8d948c4ee5f57b8ae77a
benwulf <benwulf@zedat.fu-berlin.de>
2017-05-05 20:19:12 +0200

    task1

--- task_03/ex3.r
+++ task_03/ex3.r
@@ -8,7 +8,7 @@ clinical_data <- read.csv('./data/crc_clinical_sheet.txt', sep='\t')
 # preprocessing
 
 microarray_matrix<- as.matrix(microarry_data[(-1:-2)])
-
+rownames(microarray_matrix)<-microarry_data$CLID
 
 sum_na <- function (row)
 {
@@ -28,6 +28,39 @@ plot(variance)
 sum(variance>5)
 
 top_var<-microarray_matrix_filtered[variance>5,]
-plot(density(top_var[1,]),xlim(?pc))
-for (i in 2:16){lines(density(top_var[i,]))}
+plot(density(top_var[1,]),xlim=c(-14,10),ylim=c(0,0.4),lwd=2,main='Density of Genes with variance >5',xlab="Expression-Value",col=rainbow(16)[1])
+for (i in 2:16)
+  {lines(density(top_var[i,],na.rm = T),col=rainbow(16)[i],lwd=2)}
 
+# Task 1.2
+
+# PCA not able to handle NA
+
+microarray_matrix_filtered<-microarray_matrix[(num_na<1),]
+
+pca <- prcomp(t(microarray_matrix_filtered),center=T,scale=T)
+
+
+# TO dOOOOO!
+
+# Task 1.3
+
+pc1<- pca$rotation[order(abs(pca$rotation[,1]),decreasing = T)[1:5],1]
+pc2<- pca$rotation[order(abs(pca$rotation[,1]),decreasing = T)[1:5],2]
+
+# Task 1.4
+color_key<-function(patient)
+{
+
+  return(ifelse(labels[which(labels[,1]==patient)[1],2],"blue","red"))}
+
+lables2<-list(labels$cancer)
+names(labels2)<-labels$patient
+
+paste0(strsplit("TCGA.AG.4015.01A.01R.1119.07",'\\.')[[1]][1:3], collapse = '-')
+
+pca_plot<- function (comp1, comp2)
+  {
+  plot(pca[comp2],pca[comp2],col=lapply(colnames(microarray_matrix_filtered),color_key))
+  
+  }
