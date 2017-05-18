6aeb44c7e85f5a4d92e0ccd69669f62b65755290
amnonbleich <amnonbleich@gmail.com>
2017-05-19 00:59:29 +0200

    need to produce plot and finish task 4 (problem with line numbers)

--- task_04/ex4.r
+++ task_04/ex4.r
@@ -14,7 +14,7 @@ sum_na <- function (to_sum)
 clinical_data_filtered<-clinical_data
 #Remove unneccessary columns:
 clinical_data_filtered$patient<-NULL
-clinical_data_filtered$lymphnodes_examined<-NULL
+clinical_data_processed$days_to_last_followup<-NULL
 clinical_data_filtered$vital_status<-NULL
 
 
@@ -29,22 +29,7 @@ d<-(lapply(clinical_data_filtered,class) == "factor" | lapply(clinical_data_filt
 all(d)
 # TRUE
 
-
-# TODO tune cost and gamma
-# success_rate=0
-# for (i in (1:500))
-# {
-#   smp_size <- floor(0.75 * nrow(clinical_data_filtered))
-#   training_idxs = sample(nrow(clinical_data_filtered), size = smp_size)
-#   training_data = clinical_data_filtered[training_idxs,]
-#   test_data = clinical_data_filtered[-training_idxs, ]
-#   svm_model <- svm(vascular_invasion_present ~ ., data = training_data)
-#   svm_pred <- predict(svm_model, test_data)
-#   correct = test_data$vascular_invasion_present
-#   success_rate=success_rate+sum(correct == svm_pred)/nrow(test_data)
-# }
-# success_rate=success_rate/i
-
+#Task 2
 #svm_model <- svm(vascular_invasion_present ~ ., data = training_data)
 smp_size <- floor(0.75 * nrow(clinical_data_filtered))
 training_idxs = sample(nrow(clinical_data_filtered), size = smp_size)
@@ -53,7 +38,7 @@ test_data = clinical_data_filtered[-training_idxs,]
 
 # tune using 10-fild cv, find bets gamma/cost
 set.seed(1)
-tuned = tune.svm(vascular_invasion_present~., data = training_data, gamma = seq(0.005, 0.05, 0.005), cost = 10^(-3:3), tunecontrol=tune.control(cross=10),    kernel = "linear")
+tuned = tune.svm(vascular_invasion_present~., data = training_data, gamma = seq(0.005, 0.05, 0.005), cost = 10^(0:3), tunecontrol=tune.control(cross=10),    kernel = "linear")
 # best = best.svm(vascular_invasion_present~., data = training_data, gamma = seq(0.005, 0.05, 0.005), cost = 10^(0:3), tunecontrol=tune.control(cross=10),    kernel = "linear")
 
 summary(tuned)
@@ -78,8 +63,26 @@ plot(tuned)
 # - best performance: 0.182967 error rate
 # 
 
+# test performence 
 svm_model <- svm(vascular_invasion_present ~ ., data = training_data, cost=tuned$best.parameters$cost ,gamma=tuned$best.parameters$gamma, kernel="linear")
 # remove vascular invation columns for prediction
 svm_pred <- predict(svm_model, test_data[,-ncol(test_data)])
 correct = test_data$vascular_invasion_present
 success_rate=sum(correct == svm_pred)/nrow(test_data)
+
+
+# Task 4
+col_names = names(clinical_data_filtered)
+performances<-c()
+
+for (i in (1:(ncol(clinical_data_filtered)-1)))
+{
+  tmp_data = clinical_data_filtered[,-i]
+  print (col_names[i])
+  # test performence 
+  svm_model <- svm(vascular_invasion_present ~ ., data = tmp_data, cost=tuned$best.parameters$cost ,gamma=tuned$best.parameters$gamma, kernel="linear")
+  svm_pred <- predict(svm_model, tmp_data[,-ncol(test_data)])
+  correct = test_data$vascular_invasion_present
+  success_rate_without=sum(correct == svm_pred)/nrow(test_data)
+  performances[i]=success_rate_without
+}
