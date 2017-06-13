81c99aba89b8c9865360f3b44a50d855b9fb152f
benwulf <benwulf@zedat.fu-berlin.de>
2017-06-13 18:20:51 +0200

    8

--- task_08/exercise_08.r
+++ task_08/exercise_08.r
@@ -20,3 +20,47 @@ for (i in 1:10)
   print(i)
   occ[,i]<-sapply(clone_mix_reads_seq,function(read){grepl(read,referencestrings[i])})
 }
+
+occ_bool<-as.logical(apply(occ,1,sum))
+occ_sub<-occ[occ_bool,]
+
+############
+# 3
+############
+
+
+
+Pr.R_r<-function(read_matrix,ph)
+{
+  Pr.R_r.cond.H_h<-apply(read_matrix,2,function(col){1/sum(col)})
+  
+  return(
+    apply(read_matrix,1,
+          function(row)
+          {
+            sum(
+              Pr.R_r.cond.H_h*row*ph
+            )}
+    )
+  )
+}
+
+
+own.EM <- function(occurances)
+  {
+  ph=rep(1/ncol(occurances),ncol(occurances))
+  
+  Ur=apply(occ_sub,1,sum)
+  Pr.R_r.cond.H_h<-apply(occ_sub,2,function(col){1/sum(col)})
+  for (i in 1:100)
+  {
+  # Expectation Step
+  Urh<-t(
+                        Ur*((ph_i*Pr.R_r.cond.H_h2*t(occ_sub))/Pr.R_r(occ_sub,ph))
+                        )
+  #Maximisation Step
+  ph<-apply(Urh,2,sum)/sum(Ur)
+  }
+  browser()
+  return(ph*Pr.R_r.cond.H_h)
+}
