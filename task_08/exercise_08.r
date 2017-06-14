283e18c92847119a75150de55188f394b8dc7876
benwulf <benwulf@zedat.fu-berlin.de>
2017-06-14 21:49:48 +0200

    task4

--- task_08/exercise_08.r
+++ task_08/exercise_08.r
@@ -4,6 +4,15 @@ reads<-read.fasta(file = 'fna-files/fna-files/3.TCA.454Reads.fna',as.string=TRUE
 read_Cat<- sapply(reads,function(x){if(substr(x[1],1,10)=='acgagtgcgt'){return (1)} else if(substr(x[1],1,10)=='aggctcgaca'){return (2)}else {return (3)}})
 clone_mix_reads_raw <- reads[read_Cat==1]
 clone_mix_reads_seq <- sapply(clone_mix_reads_raw,function(x){substring(x[1],11)})
+read_counts<-list()
+read_counts[unique(clone_mix_reads_seq)]=1
+for( i in clone_mix_reads_seq[duplicated(clone_mix_reads_seq)])
+{
+  read_counts[i]<-as.numeric(read_counts[i])+1
+  
+}
+U<- sapply(read_counts,function(x){x})
+
 
 references<- read.fasta('./clonal_sequences.fasta',as.string=T)
 referencestrings <-sapply(references,function(x){x[1]})
@@ -13,12 +22,12 @@ referencestrings <-sapply(references,function(x){x[1]})
 # 2
 #######
 
-occ<-matrix(nrow=length(clone_mix_reads_seq),ncol=length(referencestrings),dimnames=(list(read=names(clone_mix_reads_seq),reference=names(referencestrings))))
+occ<-matrix(nrow=length(U),ncol=length(referencestrings),dimnames=(list(read=names(U),reference=names(referencestrings))))
 
 for (i in 1:10)
 {
   print(i)
-  occ[,i]<-sapply(clone_mix_reads_seq,function(read){grepl(read,referencestrings[i])})
+  occ[,i]<-sapply(names(U),function(read){grepl(read,referencestrings[i])})
 }
 
 occ_bool<-as.logical(apply(occ,1,sum))
@@ -29,51 +38,91 @@ occ_sub<-occ[occ_bool,]
 ############
 
 
+ph_init <- apply(occ_sub,2,function(col){sum(col)/sum(occ_sub)})
 
-Pr.R_r<-function(read_matrix,ph)
-{
-  Pr.R_r.cond.H_h<-apply(read_matrix,2,function(col){1/sum(col)})
-  
-  return(
-    apply(read_matrix,1,
-          function(row)
-          {
-            sum(
-              Pr.R_r.cond.H_h*row*ph
-            )}
+
+
+our.EM<-function(read_map_matrix, ph,Ur,epsilon=1e-16)
+  {
+  Pr.R_r.cond.H_h<-apply(read_map_matrix,2,function(col)
+    {
+      if(sum(col))
+      {
+        return(1/sum(col))
+      }
+      else
+      {
+        return (0)
+      }
+    }
     )
-  )
-}
-
-
-own.EM <- function(occurances)
-  {
-  ph=rep(1/ncol(occurances),ncol(occurances))
+  read_prob_matrix <- Pr.R_r.cond.H_h*t(read_map_matrix)
+  likelyhood <-c()
   
-  Ur=apply(occ_sub,1,sum)
-  Pr.R_r.cond.H_h<-apply(occ_sub,2,function(col){1/sum(col)})
-  for (i in 1:100)
-  {
-  # Expectation Step
-  Urh<-t(
-                        Ur*((ph_i*Pr.R_r.cond.H_h2*t(occ_sub))/Pr.R_r(occ_sub,ph))
-                        )
-  #Maximisation Step
-  ph<-apply(Urh,2,sum)/sum(Ur)
-  }
-  browser()
-  return(ph*Pr.R_r.cond.H_h)
-}
-
-
-H = names(referencestrings)
-for (i in H)
-{
-  print("borisssssssssssssssss")
-  print(i)
-  R = names(occ_sub[occ_sub[,i]==T,i])
-  for (j in R)
-  {
+  
+  while(T)
+    {
+    Pr.R_r<-apply(read_map_matrix,1,
+                  function(row)
+                  {
+                    sum(
+                      Pr.R_r.cond.H_h*row*ph
+                    )})
+   
     
+    numerator<- (ph*read_prob_matrix)
+    
+    U_rh<- t(numerator)*(Ur/Pr.R_r)
+  
+    ph_head<-apply(U_rh,2,sum)/sum(Ur)
+    if(sum(is.nan(ph_head)))
+    {browser()}
+
+    likelyhood<-c(likelyhood,sum(Ur*log(Pr.R_r)))
+    if(all(abs(ph-ph_head)<epsilon))
+    {
+      return(list(ph=ph_head,likelyhoods=likelyhood,epsilon=epsilon))
+    }
+    else
+    {
+      ph=ph_head
+      }
+  
+    
+    }
+  
   }
+  
+
+
+
+
+
+u_sub <- U[names(U) %in% rownames(occ_sub)]
+Ur <-u_sub[order(names(u_sub))]
+occ_sub<-occ_sub[order(rownames(occ_sub)),]
+
+
+a=our.EM(occ_sub,ph_init,Ur)
+
+
+bootres<-list()
+
+
+for (i in 1:1000)
+{ print(i)
+  samples<-sample(nrow(occ_sub),nrow(occ_sub),replace=T)
+  bootstrap_sub<-occ_sub[sort(unique(samples)),]
+  
+  count<- table(samples)
+  newUr<- as.vector(count)
+  names(newUr)<-names(occ_sub)[as.numeric(names(count))]
+  
+  ph_i<- rep(1/10,10)
+  
+  r=our.EM(bootstrap_sub,ph_i,newUr,1e-5)
+
+  bootres[i]<-list(r$ph)
 }
+
+
