0123ce7260e81be7b0dafc92e59938899e3c5fa5
benwulf <benwulf@zedat.fu-berlin.de>
2017-06-15 11:20:39 +0200

    nearly finished#

--- task_08/exercise_08.r
+++ task_08/exercise_08.r
@@ -1,4 +1,5 @@
 library(seqinr)
+library(boot)
 reads<-read.fasta(file = 'fna-files/fna-files/3.TCA.454Reads.fna',as.string=TRUE)
 
 read_Cat<- sapply(reads,function(x){if(substr(x[1],1,10)=='acgagtgcgt'){return (1)} else if(substr(x[1],1,10)=='aggctcgaca'){return (2)}else {return (3)}})
@@ -105,24 +106,48 @@ occ_sub<-occ_sub[order(rownames(occ_sub)),]
 
 a=our.EM(occ_sub,ph_init,Ur)
 
+barplot(a$ph,las=2,ylab='Properbility',main='Properbilitys of haplotypes for the dataset (337 reads)')
 
-bootres<-c()
+plot(a$likelyhoods,ylab='likelyhood',xlab='iteration',type='l')
 
 
-for (i in 1:1000)
-{ print(i)
-  samples<-sample(nrow(occ_sub),nrow(occ_sub),replace=T)
-  bootstrap_sub<-occ_sub[sort(unique(samples)),]
+bootstrap_resample_dataset <-function(data,indeces)
+{ 
+  bootstrap_sub<-data[sort(unique(indeces)),]
   
-  count<- table(samples)
+  count<- table(indeces)
   newUr<- as.vector(count)
-  names(newUr)<-names(occ_sub)[as.numeric(names(count))]
+  names(newUr)<-names(data)[as.numeric(names(count))]
   
   ph_i<- rep(1/10,10)
   
   r=our.EM(bootstrap_sub,ph_i,newUr,1e-5)
 
-  bootres<-c(bootres,r$ph)
+  return(r$ph)
 }
+
+bootobj<- boot(occ_sub,bootstrap_resample_dataset,1000,parallel ="multicore",ncpus=4)
+
+res= matrix(ncol=3,nrow=ncol(occ_sub),dimnames=list(haplotypes=colnames(occ_sub),value=c('mean','conf_intervall_low_95','conf_intervall_up_95')))
+
+res<-c()
+for (i in 1:10)
+{
+  conf_int<-boot.ci(bootobj,type='basic',index=i)
+  res<- c(res,mean(bootobj$t[,i]),conf_int$basic[4:5])
+  
+}
+r= matrix(res,ncol=3,dimnames=list(haplotypes=colnames(occ_sub),value=c('mean','conf_intervall_low_95','conf_intervall_up_95')))
+
+print(r)
+
+#############
+# 5
+#############
+
+## How reliable are results based on the bootstrapping?
+
+boxplot(bootobj$t,xlab='haplotypes',ylab='distribution',col=c('white','red','red','white','red','red','white','white','white','white'))
+
 
 
